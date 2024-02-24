854a6bb2400e5b62de5e4bb918f8b178ac4817a0
Krishi Gandhi <krishigandhi2601@gmail.com>
2024-02-24 06:46:36 +0000

    date & time

---
+++ 01_basics/02_practice.js
@@ -0,0 +1,10 @@
+let date = new Date()
+// console.log(date)
+
+// console.log(date.toString())
+
+// let updateDate = new Date(2023 , 0, 26)
+// console.log(updateDate.toDateString())
+
+let updateDate = new Date(2023,0,26, 5,3)
+console.log(updateDate.toLocaleString()) 
