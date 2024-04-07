6c70a6f210efcb87803d9c7b5e6bb5e0f76678cf
krishigandhi <98482670+krishigandhi@users.noreply.github.com>
2024-04-07 15:58:03 +0000

    bmi calac

--- Projects/Color changer/file.js
+++ Projects/Color changer/file.js
@@ -24,3 +24,31 @@ buttons.forEach(function (button){
         }
     })
 });
+//Switch Case
+// const buttons = document.querySelectorAll('.button');
+// const body = document.querySelector('body');
+
+// buttons.forEach(function (button){
+//     console.log(button);
+//     button.addEventListener('click',function(e){
+//         console.log(e);
+//         console.log(e.target);
+//         switch (e.target.id) {
+//             case 'black':
+//                 body.style.backgroundColor = e.target.id;
+//                 break;
+//             case 'grey':
+//                 body.style.backgroundColor = e.target.id;
+//                 break;
+//             case 'red':
+//                 body.style.backgroundColor = e.target.id;
+//                 break;
+//             case 'yellow':
+//                 body.style.backgroundColor = e.target.id;
+//                 break;
+//             default:
+//                 // Handle default case here if needed
+//                 break;
+//         }
+//     });
+// });
