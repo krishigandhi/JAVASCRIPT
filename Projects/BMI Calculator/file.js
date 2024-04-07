b4086c70e90d559b85d05354197f4ff681197435
krishigandhi <98482670+krishigandhi@users.noreply.github.com>
2024-04-07 17:07:24 +0000

    bmi calc

--- Projects/BMI Calculator/file.js
+++ Projects/BMI Calculator/file.js
@@ -5,14 +5,25 @@ document.addEventListener('submit',function(e){
   const height =parseInt( document.querySelector('#height').value );
   const weight =parseInt( document.querySelector('#weight').value );
   const results = document.querySelector('#results');
+  
   if(height === '' || height < 0 || isNaN(height) ){
     results.innerHTML = (`Enter valid height ${height}`);
   }
   else if(weight === '' || weight < 0 || isNaN(weight) ){
     results.innerHTML = (`Enter valid weight ${weight}`);
   }
+
   else{
     const bmi = (weight/((height*height)/10000)).toFixed(2);
-    results.innerHTML = `<span>${bmi}</span>`
+   results.innerHTML = `<span>${bmi}</span>`
+    if(bmi<18.6){
+      results.innerHTML = `${bmi} You are under Weight`;
+    }
+    if(bmi> 18.6 && bmi<24.9){
+      results.innerHTML= `${bmi} You are in normal range`
+    }
+    if(bmi>24.9){
+      results.innerHTML = `${bmi} YOu are overweight`
+    }
   }
 });
