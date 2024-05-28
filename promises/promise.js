abeae2167acaf8a69663f6a0c0c55b1695a208db
krishigandhi <98482670+krishigandhi@users.noreply.github.com>
2024-05-28 11:44:15 +0000

    promises

---
+++ promises/promise.js
@@ -0,0 +1,14 @@
+// const p = new Promise(function(resolve,reject){
+//     setTimeout()
+// })
+
+
+// promise.then(() => {}) => used when promise is fullfilled or resolved 
+// promise.catch() => used when primise is rejected 
+
+async function heyy(){
+    console.log('hello');
+}
+
+
+//Async - Await
