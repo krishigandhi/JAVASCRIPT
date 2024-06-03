be348faaabba0f6584bf001d35546c6c98324cc1
Krishi Gandhi <krishigandhi2601@gmail.com>
2024-06-03 13:30:40 +0000

    fetch api

--- Fetch API/file.js
+++ Fetch API/file.js
@@ -1,7 +1,11 @@
 let URL = "https://cat-fact.herokuapp.com/facts" ;
- 
+ const display = document.querySelector('#facts');
+ const btn = document.querySelector('#btn');
  const getFacts = async() => {
     console.log("getting data")
     let response = await fetch(URL);
-    response.json();
+   let data = await response.json();
+   console.log(data[1].text)
+   display.innerText = (data[2].text);
  };
+ btn.addEventListener('click',getFacts);
