552e02130cec75fd5f0e0e719fc3d6c4443917f1
Krishi Gandhi <krishigandhi2601@gmail.com>
2024-06-09 11:50:35 +0000

    promise

--- promises/promise.js
+++ promises/promise.js
@@ -12,3 +12,24 @@ async function heyy(){
 
 
 //Async - Await
+
+let pro = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async Task");
+        resolve();
+    },1000)
+})
+
+pro.then(function(){
+    console.log('resolved 1 ')
+})
+
+// Same but it can also be done ww=ithout variable name 
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async Task");
+        resolve()
+    },1000)
+}).then(function(){
+    console.log('resolved 2')
+})
