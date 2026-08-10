a804693f474d6de6d6867b6ecd75cc676f857c28
Krishi Gandhi <98482670+krishigandhi@users.noreply.github.com>
2026-08-10 17:06:06 +0000

    commit

--- Advance-JS/index.js
+++ Advance-JS/index.js
@@ -81,4 +81,4 @@ console.log(`What is the capital of India ?`)*/
 // else throw new Error("Num is less than 4");
 // console.log("Finished")
 
-
+//Hoisting //
---
+++ Stock Ticker/index.js
@@ -0,0 +1,15 @@
+function getData(){
+    return{
+        name: 'QtechAI',
+        sym: 'QTA',
+        price: Math.random(),
+        time: new Date()
+    }
+}
+for(let i=0;i< 10 ;i++){
+    console.log((Math.random()*4).toFixed(2))
+
+}
+
+let date = new Date()
+console.log(date)
