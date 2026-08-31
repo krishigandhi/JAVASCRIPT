6467586774b5e55a1af8e83f21b8d0abfea68cfc
Krishi Gandhi <98482670+krishigandhi@users.noreply.github.com>
2026-08-31 14:00:47 +0000

    added symbols

--- Stock Ticker/index.js
+++ Stock Ticker/index.js
@@ -1,19 +1,30 @@
 import { getStockData } from "./fakeStockAPI.js"
+
+let prevPrice = null
+
 setInterval(function(){
     const stockData = getStockData()
     renderStockTicker(stockData)
-},1500)
+}, 1500)
+
 function renderStockTicker(stockData){
-const stocKname = document.getElementById('name')
-const stocKsym =  document.getElementById('sym')
-const stocKprice = document.getElementById('price')
-const stocKtime = document.getElementById('time')
 
-const{name,sym, price,time } = stockData
+    const stocKname = document.getElementById('name')
+    const stocKsym = document.getElementById('sym')
+    const stocKprice = document.getElementById('price')
+    const stocKtime = document.getElementById('time')
 
+    const { name, sym, price, time } = stockData
 
-stocKname.innerHTML = `Name :  ${name}`;
-stocKsym.innerHTML =  `Symbol: ${sym}`
-stocKprice.innerHTML = `Price :${price}`
-stocKtime.innerHTML = `Time :${time}`
+    const priceDirectionIcon =
+        price > prevPrice ? '^' :
+        price < prevPrice ? 'v' :
+        '>'
+
+    stocKname.innerHTML = `Name : ${name}`
+    stocKsym.innerHTML = `Symbol : ${sym}`
+    stocKprice.innerHTML = `Price : ${price} ${priceDirectionIcon}`
+    stocKtime.innerHTML = `Time : ${time}`
+
+    prevPrice = price
 }
