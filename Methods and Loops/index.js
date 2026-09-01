f4fd5a565249486050c8ff2dd446d96008909b6b
Krishi Gandhi <98482670+krishigandhi@users.noreply.github.com>
2026-09-01 16:58:25 +0000

    forEach

--- Methods and Loops/index.js
+++ Methods and Loops/index.js
@@ -1,9 +1,16 @@
 let letters =  {
-    a: "aa",
-    b: "bb"
+    a: 2,
+    b: 3
 
 }
-
+let nums = [1,2]
+//////////////////For in /////////////////////////////////////////
 for(const name in letters){
     console.log( letters[name])
 }
+///////////////// For Each ///////////////////////////////////
+nums.forEach(num => {
+    let sum = num*2
+    console.log(sum)// This 
+});
+
